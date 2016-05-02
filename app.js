a9c0b6f6a46996a119053155e1bc7874513430d0
Sharon Rolel <sharonr@tipranks.com>
2016-05-02 20:39:44 +0300

    initial

---
+++ app.js
@@ -0,0 +1,5 @@
+import flow from 'lodash/flow';
+import * as flow2 from 'lodash/flow';
+
+console.log('import flow from lodash/flow:', flow);
+console.log('import * as flow from lodash/flow:', flow2);
